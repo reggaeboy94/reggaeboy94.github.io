b65ea7859156ab3047f62688946e35889958037c
Bartek Wróbel <bwrobell@op.pl>
2019-01-29 17:44:26 +0100

    Slider and php form solution

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$mailFrom = $_POST['mail'];
+	$message = $_POST['message'];
+
+
+	$subject = "Wiadomość ze strony internetowej";
+	$mailTo ="bwrobell@op.pl";
+	$headers = "Od: ".$mailFrom;
+	$txt = "Otrzymałeś wiadomość od ".$name ".\n\n".message; 
+
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: contact.html?mailsend");
+}
---
+++ script.js
@@ -0,0 +1,21 @@
+// $(".dnSlide-main").dnSlide({
+//     "isOddShow" : false,
+//     "width": 600,
+//     "height": 800,
+//     "dnSlideFirstWidth": 800,
+//     "dnSlideFirstHeight": 234,
+//     "autoPlay": false,
+//     "delay": 5000,
+//     "scale": 0.9,
+//     "speed": 500,
+//     "verticalAlign": "middle",
+//     "afterClickBtnFn": null
+// });
+
+$(".dnSlide-main").dnSlide('show');
+
+$(function(){
+
+  $(".dnSlide-main").dnSlide();
+
+});
